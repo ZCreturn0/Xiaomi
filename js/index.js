2c1183269236648552b0f43ecd4b32859c45f426
ZCreturn0 <237350543@qq.com>
2016-12-12 17:51:32 +0800

    xiaomi

--- js/index.js
+++ js/index.js
@@ -12,4 +12,55 @@ function init()
 		$('.shopping img').attr("src", "images/购物车.svg");
 		$('.shopping-list').stop().slideUp(250);
 	});
+	$('.header-list-item').css('width',$('.wrapper').width());
+//	productsShow();
+}
+
+function productsShow()
+{
+	productsSlideDown();
+	productsSlideUp();
+}
+
+function productsSlideDown()
+{
+	$('#xiaomi,#red_mi,#pad,#tv,#box,#router,#hardware,.header-list-item').mouseover(function(){
+		var index = $(this).attr('index');
+	//	console.log(index);
+		selectProductsItem(index);
+		$('.header-list-item').stop(true)
+		.animate({'height':'229px'},
+		(229-$('.header-list-item').height())/229*200,'linear');
+	});
+}
+
+function productsSlideUp()
+{
+	$('#xiaomi,#red_mi,#pad,#tv,#box,#router,#hardware,.header-list-item').mouseout(function(){
+		$('.header-list-item').stop(true)
+		.animate({'height':'0px'},
+		($('.header-list-item').height())/229*200,'linear');
+	});
+}
+
+function selectProductsItem(index)
+{
+//	console.log(index);
+	switch(index)
+	{
+		case "1":xiaoMi();break;
+		case "2":break;
+		case "3":break;
+		case "4":break;
+		case "5":break;
+		case "6":break;
+		default:;
+	}
+}
+
+function xiaoMi()
+{
+//	$('.product-pic:eq(0)').append($('<img></img>'));
+	$('.product-pic:eq(0) img').attr('src','images/xiaomiNOTE2-320-220!320x220.jpg');
+//	console.log($('.products-entrance').children().eq(0).children().eq(1));
 }
